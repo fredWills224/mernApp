b967aa7b894c7976a8b8fcad6c0b40ca61608d32
fredWills224 <fredwills4229@gmail.com>
2020-11-20 02:21:53 -0500

    get/fetch data from mongoDB + display in react-app

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,33 @@ class App extends React.Component{
 
   state ={
     title: '',
-    body: ''
-  }
+    body: '',
+    posts: []
+  };
+
+  componentDidMount = ()=>{
+    this.getBlogPost();
+  };
+
+  getBlogPost = ()=>{
+
+    axios.get('/api')
+      .then((response)=>{
+        const data = response.data;
+        this.setState({ posts: data });
+        console.log('Data has been received!!!!');
+      })
+      .catch(()=>{
+        console.log('Error retrieving data!!!!');
+        alert('Error retrieving data!!!!');
+      });
+    ;
+
+  };
 
   handleChange = ({ target })=>{
+    
     const { name, value } = target;
-
     this.setState({
       //[name] dynamicly passes in 'title' from input and 'body' from textArea 
       [name]: value
@@ -19,12 +40,13 @@ class App extends React.Component{
   };
 
   submit = (event)=>{
+    
     //stoping the browser's behavior, keeping it from refreshing
     event.preventDefault();
     const payload = {
       title: this.state.title,
       body: this.state.body
-    }
+    };
 
     axios({
       //[http://localhost:8080] no longer needed in url because it has been added
@@ -37,6 +59,7 @@ class App extends React.Component{
       .then(()=>{
         console.log('Data has been sent to the server');
         this.resetUserInputs();
+        this.getBlogPost();
       })
       .catch(() =>{
         console.log('Internal server error');
@@ -46,11 +69,25 @@ class App extends React.Component{
   };
 
   resetUserInputs = ()=>{
+    
     this.setState({
       title: '',
       body:''
     });
-  }
+  
+  };
+
+  displayBlogPost = (posts)=>{
+
+    if(!posts.length) return null;
+    return posts.map((post, index)=>(
+      <div key={index}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+
+  };
 
   render(){
 
@@ -87,11 +124,15 @@ class App extends React.Component{
           <button>Submit</button>
 
         </form>
-      
+
+        <div className='blog-post'>
+          {this.displayBlogPost(this.state.posts)}
+        </div>
+
       </div>
     );
 
-  }
+  };
 
 }
 
